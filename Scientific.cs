d685cdd8d719bd68a709ab175290426c41cdb29f
282355 <282355@ST-34948>
2026-04-24 13:03:43 -0500

    gfghfhyj

--- Scientific.cs
+++ Scientific.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,31 +10,66 @@ namespace boxMos
 {
     public class Scientific
     {
-        public static List<(Term, string)> Addition(List<Term> terms) // 2 + 2 + x 
+        public static List<(Term, string)> Addition(List<Term> terms) // 2x + x + 3 + 7
         {
             List<(Term, string)> added = new List<(Term, string)>();
+            List<double> definedvar = new List<double>(); 
 
-            double likeTerm_Constant = 0;
-            ConstantTerm likeTerm_Var;
+
             int son = 0;
-            foreach (Term term in terms) 
+            foreach (Term term in terms)
             {
                 if (term.HasVariable)
                 {
-                    added.Add((term, term.Variables.ToList()[son].Item1)); // (2, ConstantTerm), (2, ConstantTerm), (x, VariableTerm)
+                    added.Add((term, term.Variables.ToList()[son].Item1));
+                    definedvar.Add(term.Variables.ToList()[son].Item2);
                 }
                 else
                 {
-                    added.Add((term, "0")); // (2, ConstantTerm), (2, ConstantTerm), (x, VariableTerm)
+                    added.Add((term, ""));
+                    definedvar.Add(1);
                 }
                 son++;
-            }
-            added = added.OrderBy(t => t.Item2).ToList(); // 2 + 2 + x
-            
+            } // 2x, x, 3, 7 -> (2x, "x"), (x, "x"), (3, ""), (7, "")
+            added = added.OrderBy(t => t.Item2).ToList(); // 2x + x + 3 + 7 / 3 + 7 + 2x + x
+
+            List<string> listofvars = new List<string>();
             for (int i = 0; i < added.Count; i++)
             {
-                
+                if (listofvars.Contains(added[i].Item2))
+                {
+
+                }
+                else
+                {
+                    listofvars.Add(added[i].Item2);
+                }
             }
+
+            List<(ConstantTerm, string)> finalAdded = new List<(ConstantTerm, string)>();
+            foreach (string var in listofvars)
+            {
+                for (int i = added.FindIndex(t => t.Item2 == var); i < added.FindLastIndex(t => t.Item2 == var); i++) // now that we have the list of terms ordered by variable, we now split that list into chunks of varibales. eg: line 30 becomes { 2x, x } and { 3, 7 }.
+                {
+                    finalAdded.Add((added[i].Item1 as ConstantTerm, added[i].Item2));
+                }
+                for (int tung = 0; tung < finalAdded.Count; tung++)
+                {
+                    ConstantTerm sahur;
+                    if (finalAdded[tung].Item2 == "")
+                    {
+                        if (tung == 0)
+                        {
+                            sahur = finalAdded[tung].Item1;
+                        }
+                        else
+                        {
+                            sahur = new ConstantTerm(finalAdded[tung].Item1.Coefficient + sahur.Coefficient, [("", definedvar[tung])]);
+                        }
+                    }
+                }
+            }
+
 
             return added;
 
